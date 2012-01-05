8a5910eb6e24c8026df32323bfc3799a31601472
caljp13 <soad89toxicity@yahoo.com.br>
2012-01-05 20:14:04 -0200

    Ajustes

--- replaceSpecialChar.js
+++ replaceSpecialChar.js
@@ -5,7 +5,7 @@
 * @version 1.1
 * @date 2011-01-05
 */
-if(typeof(String.prototype.replaceSpecialChars)!=="function") String.prototype.replaceSpecialChars=function(){var _replace={"ç":"c","æ":"ae","œ":"oe","á":"a","é":"e","í":"i","ó":"o","ú":"u","à":"a","è":"e","ì":"i","ò":"o","ù":"u","ä":"a","ë":"e","ï":"i","ö":"o","ü":"u","ÿ":"y","â":"a","ê":"e","î":"i","ô":"o","û":"u","å":"a","ã":"a","ø":"o","u":"u","Á":"A","É":"E","Í":"I","Ó":"O","Ú":"U","Ê":"E","Ô":"O","Ü":"U","Ã":"A","Õ":"O","À":"A","Ç":"C"};return this.replace(/[à-ú]/g,function(a){console.log(a); if(typeof(_replace[a])!="undefined") return _replace[a]; return a;});};
+if(typeof(String.prototype.replaceSpecialChars)!=="function") String.prototype.replaceSpecialChars=function(){var _replace={"ç":"c","æ":"ae","œ":"oe","á":"a","é":"e","í":"i","ó":"o","ú":"u","à":"a","è":"e","ì":"i","ò":"o","ù":"u","ä":"a","ë":"e","ï":"i","ö":"o","ü":"u","ÿ":"y","â":"a","ê":"e","î":"i","ô":"o","û":"u","å":"a","ã":"a","ø":"o","u":"u","Á":"A","É":"E","Í":"I","Ó":"O","Ú":"U","Ê":"E","Ô":"O","Ü":"U","Ã":"A","Õ":"O","À":"A","Ç":"C"};return this.replace(/[à-ú]/g,function(a){if(typeof(_replace[a])!="undefined") return _replace[a]; return a;});};
 
 // Exemplo de uso
 "ógãos".replaceSpecialChars(); // saída: orgaos
