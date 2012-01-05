56544ec8263ab935189712807d067ed245f77ef0
caljp13 <soad89toxicity@yahoo.com.br>
2012-01-05 18:06:05 -0200

    Nova funcao para substituicao de caracteres especias com JavaScript

---
+++ replaceChar.js
@@ -0,0 +1,9 @@
+/*
+* @author Carlos Vinicius
+* @version 1.0
+* @date 2011-01-05
+*/
+if(typeof(String.prototype.replaceChar)!=="function") String.prototype.replaceChar=function(){var _replace={"ç":"c","æ":"ae","œ":"oe","á":"a","é":"e","í":"i","ó":"o","ú":"u","à":"a","è":"e","ì":"i","ò":"o","ù":"u","ä":"a","ë":"e","ï":"i","ö":"o","ü":"u","ÿ":"y","â":"a","ê":"e","î":"i","ô":"o","û":"u","å":"a","ã":"a","ø":"o","u":"u","Á":"A","É":"E","Í":"I","Ó":"O","Ú":"U","Ê":"E","Ô":"O","Ü":"U","Ã":"A","Õ":"O","À":"A","Ç":"C"};return this.replace(/[à-ú]/g,function(a){console.log(a); if(typeof(_replace[a])!="undefined") return _replace[a]; return a;});};
+
+// Exemplo de uso
+"ógãos".replaceChar(); // saída: orgaos
