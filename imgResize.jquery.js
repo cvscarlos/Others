e9be4cb978e7623cce1bdbd934453f09f65fe70f
caljp13 <soad89toxicity@yahoo.com.br>
2012-01-08 12:48:03 -0200

    Versão 2 do plugin Image Resize

--- imgResize.jquery.js
+++ imgResize.jquery.js
@@ -6,6 +6,8 @@
 * @date 2011-01-06
 *
 * @usage: $("img").resize({"maxWidth":250});
+*         Para que funcione em navegadores com Webkit é necessário adicionar esta classe a imagem que será redimensionada
+*         <img src="exemple.jpg" class="imgResizeSetSize" />
 *
 * This work is licensed under the Creative Commons Attribution 3.0 Unported License. To view a copy of this license,
 * visit http://creativecommons.org/licenses/by/3.0/ or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
